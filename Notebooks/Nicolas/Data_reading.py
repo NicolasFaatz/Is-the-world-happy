d7491496ac5826030865ad2747aa48e5269b3248
NicolasFaatz <154017530+NicolasFaatz@users.noreply.github.com>
2024-03-14 18:03:22 +0100

    Add files via upload

--- Notebooks/Nicolas/Data_reading.py
+++ Notebooks/Nicolas/Data_reading.py
@@ -2,6 +2,8 @@ import pandas as pd
 from datetime import datetime
 import pycountry
 import geopandas
+import seaborn as sn
+import matplotlib.pyplot as plt
 
 def datacutting(x):
     name1="WeatherEvents_Jan2016-Dec2022.csv"
@@ -51,12 +53,44 @@ df=pd.read_csv("2015.csv")
 #df["Economy (GDP per Capita)"]=df["Economy (GDP per Capita)"].astype(float)
 #filt=df["Economy (GDP per Capita)"]==df["Economy (GDP per Capita)"].min()
 #print(df[filt],df.tail())
+"""
+g=sn.PairGrid(df)
+g.map(sn.scatterplot)
+plt.savefig("scatterplot",bbox_inches="tight",dpi=1200)
+plt.show()
+"""
+df2=df.drop(columns=["Country","Region","Dystopia Residual","Happiness Rank", "Standard Error"])
+correlation=df2.corr(method='spearman')
+sn.heatmap(correlation,vmin=-1, vmax=1, center=0, cmap="viridis", annot=True)
+plt.show()
+x=[]
+y=[0,1]
+for i in df["Happiness Rank"]:
+    if i<80:
+        x.append(0)
+    else:
+        x.append(1)
+df2.loc[:,"target"] =x
 
+"""
+g=sn.FacetGrid(df,hue="target")
+g.map(sn.scatterplot, "Economy (GDP per Capita)","Family")
+g.add_legend()
+plt.grid(True)
+"""
+"""
+g=sn.PairGrid(df2,hue="target")
+g.map(sn.scatterplot)
+#plt.savefig("scatterplot",bbox_inches="tight",dpi=1200)
+plt.show()
+"""
+sn.pairplot(df2,hue="target")
+"""
 df['CODE']=alpha3code(df["Country"])
 world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
 world.rename(columns={'iso_a3':"CODE"},inplace=True)
 #world.columns=['pop_est', 'continent', 'name', 'CODE', 'gdp_md_est', 'geometry']
-print(world.columns,df.columns)
+
 # then merge with our data 
 merge=pd.merge(world,df,on='CODE')
 #merge.plot(column='Happiness Rank', scheme="quantiles",figsize=(25, 20),legend=True,cmap='coolwarm')
@@ -64,4 +98,4 @@ merge.plot(column='Happiness Rank', scheme="quantiles",figsize=(25, 20),legend=T
 #print(df.head())
 #df2=df.drop(columns=["TimeZone","County","AirportCode","ZipCode"])
 #print(df.isnull().sum(),len(df["Country"]))
-
+"""
